ce941aa5529cecfe610744f1ad8f932e964b451c
Arthur Gramm <arthurgramm@outlook.com.br>
2021-09-08 16:50:40 -0300

    update env config to production and development

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:3000/api',
-  baseURL: 'http://localhost:3333/api/v1/',
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_PROD_API_URL
+      : process.env.REACT_APP_DEV_API_URL,
 });
 
 export default api;
